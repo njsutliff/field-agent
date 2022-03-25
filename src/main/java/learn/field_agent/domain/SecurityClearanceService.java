f6f73b7a09139c1418037a4e9b74c34b7f09e394
njsutliff <njsutliff@gmail.com>
2022-03-25 15:20:00 -0400

    filling out validate and add() method should be done

--- src/main/java/learn/field_agent/domain/SecurityClearanceService.java
+++ src/main/java/learn/field_agent/domain/SecurityClearanceService.java
@@ -1,34 +1,57 @@
 package learn.field_agent.domain;
 
 import learn.field_agent.data.SecurityClearanceRepository;
-import learn.field_agent.models.Agent;
 import learn.field_agent.models.SecurityClearance;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Service
 public class SecurityClearanceService {
-    private  final SecurityClearanceRepository repository;
+    private final SecurityClearanceRepository repository;
 
     public SecurityClearanceService(SecurityClearanceRepository repository) {
         this.repository = repository;
     }
 
-    List<SecurityClearance> findAll(){
-        return  repository.findAll();
+    public List<SecurityClearance> findAll() {
+        return repository.findAll();
     }
-    SecurityClearance findById(int securityClearanceId){
+
+    public SecurityClearance findById(int securityClearanceId) {
         return repository.findById(securityClearanceId);
     }
-    SecurityClearance add(SecurityClearance sc){
-        return sc;
-    }
-    Result<SecurityClearance> update(SecurityClearance sc){
-        Result<SecurityClearance> result = new Result<>();
+
+    public Result<SecurityClearance> add(SecurityClearance sc) {
+        Result<SecurityClearance> result = validate(sc);
+        if (!result.isSuccess()) {
+            result.addMessage("Unable to add", ResultType.INVALID);
+            return result;
+        }
+        result.setPayload(repository.add(sc));
         return result;
     }
-    Result<SecurityClearance> deleteById(int securityClearanceId){
+
+    public Result<SecurityClearance> update(SecurityClearance sc) {
+        Result<SecurityClearance> result = validate(sc);
+        return result;
+    }
+
+    boolean deleteById(int securityClearanceId) {
+        return false;
+    }
+
+    private Result<SecurityClearance> validate(SecurityClearance toValidate) {
         Result<SecurityClearance> result = new Result<>();
+        if(Validations.isNullOrBlank(toValidate.getName()){
+            result.addMessage("Security clearance name required.", ResultType.INVALID);
+        }
+        findAll().stream()
+                .filter(securityClearance -> securityClearance.getName()
+                .equals(toValidate.getName()))
+                .findAny().ifPresent(toNotFind -> result
+                .addMessage("Name cannot be duplicated.", ResultType.INVALID));
         return  result;
     }
 }

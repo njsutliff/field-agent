26a7973ce8929d04d05497cfa543af5d46f7d11c
njsutliff <njsutliff@gmail.com>
2022-03-25 12:30:07 -0400

    add in repo added

--- src/main/java/learn/field_agent/data/SecurityClearanceJdbcTemplateRepository.java
+++ src/main/java/learn/field_agent/data/SecurityClearanceJdbcTemplateRepository.java
@@ -3,8 +3,12 @@ package learn.field_agent.data;
 import learn.field_agent.data.mappers.SecurityClearanceMapper;
 import learn.field_agent.models.SecurityClearance;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Repository;
 
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.List;
 
 @Repository
@@ -37,7 +41,18 @@ public class SecurityClearanceJdbcTemplateRepository implements SecurityClearanc
 
     @Override
     public SecurityClearance add(SecurityClearance sc) {
-        return null;
+        final String sql = "insert into security_clearance (security_clearance_id,, security_clearance_name) "
+                + " values (?,?);";
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        int rowsAffected = jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, sc.getSecurityClearanceId());
+            ps.setString(2, sc.getName());
+            return ps;
+        }, keyHolder);
+        if (rowsAffected<=0){ return  null; }
+        sc.setSecurityClearanceId(keyHolder.getKey().intValue());
+        return  sc;
     }
 
     @Override

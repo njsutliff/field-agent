d1eaf354e8bf9fd257c8632b2aa73d39d1394771
njsutliff <njsutliff@gmail.com>
2022-03-29 14:51:41 -0400

    update and deleteById added in alias service, should be almost identical to security clearance methods, add is the hard part

---
+++ src/main/java/learn/field_agent/domain/AliasService.java
@@ -0,0 +1,69 @@
+package learn.field_agent.domain;
+
+import learn.field_agent.data.AliasRepository;
+import learn.field_agent.models.Agent;
+import learn.field_agent.models.Alias;
+import org.springframework.transaction.annotation.Transactional;
+
+public class AliasService {
+
+    private final AliasRepository repository;
+
+    public AliasService(AliasRepository repository) {
+        this.repository = repository;
+    }
+
+    public Alias findByAgentId(int agentId){
+        return  repository.findByAgentId(agentId);
+    }
+
+    public Result<Alias> add(Alias alias){
+        Result<Alias> result = validate(alias);
+        if(!result.isSuccess()){
+            return result;
+        }
+        if (alias.getId()!=0){
+            result.addMessage("Alias id cannot be set during add", ResultType.INVALID);
+            return result;
+        }
+        // require persona if name duplicated
+        alias = repository.add(alias);
+        result.setPayload(alias);
+        return  result;
+    }
+
+
+
+    public Result<Alias> update(Alias agent){
+        Result<Alias> result = validate(agent);
+        if (!result.isSuccess()) {
+            return result;
+        }
+
+        if (agent.getAgentId() <= 0) {
+            result.addMessage("agentId must be set for `update` operation", ResultType.INVALID);
+            return result;
+        }
+
+        if (!repository.update(agent)) {
+            String msg = String.format("agentId: %s, not found", agent.getAgentId());
+            result.addMessage(msg, ResultType.NOT_FOUND);
+        }
+
+        return result;
+    }
+
+    public boolean deleteById(int aliasId){
+        return repository.deleteById(aliasId);
+    }
+    private Result<Alias> validate(Alias alias) {
+        Result<Alias> result = new Result<>();
+        if(alias == null){
+            result.addMessage("alias cannot be null", ResultType.INVALID);
+            return result;
+        }
+        if (Validations.isNullOrBlank(alias.getAlias()){
+            result.addMessage("Name is required", ResultType.INVALID);
+        }
+    }
+}

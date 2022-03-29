b7a72978e31292d0b5faab1a9645a7e841097234
njsutliff <njsutliff@gmail.com>
2022-03-29 14:59:20 -0400

    alias repo test setup, now to implement tests

--- src/test/java/learn/field_agent/data/AliasJdbcTemplateRepositoryTest.java
+++ src/test/java/learn/field_agent/data/AliasJdbcTemplateRepositoryTest.java
@@ -1,4 +1,35 @@
 package learn.field_agent.data;
 
+import learn.field_agent.models.Alias;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class AliasJdbcTemplateRepositoryTest {
+    @Autowired
+    AliasJdbcTemplateRepository repository;
+    @Autowired
+    KnownGoodState knownGoodState;
+
+    @BeforeEach
+    void setup() {
+        knownGoodState.set();
+    }
+    @Test
+    void shouldAdd() {
+    }
+
+    @Test
+    void shouldFindByAgentId() {
+        Alias test = new Alias(1, "test", "testPersona", 1);
+         repository.findByAgentId(1);
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void deleteById() {
+    }
 }

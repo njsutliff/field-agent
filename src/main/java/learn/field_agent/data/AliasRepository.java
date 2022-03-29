e367b0f27b4f1ef419d0946802a7f1fc482e6638
njsutliff <njsutliff@gmail.com>
2022-03-29 14:38:29 -0400

    cleaned up alias repo, now to alias service

--- src/main/java/learn/field_agent/data/AliasRepository.java
+++ src/main/java/learn/field_agent/data/AliasRepository.java
@@ -2,17 +2,11 @@ package learn.field_agent.data;
 
 import learn.field_agent.models.Alias;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-
-public interface AliasRepository {
-
-    Alias  findByAgentId(int agentId);
-
-    Alias add(Alias alias);
-
-    boolean update(Alias agent);
-    @Transactional
-    boolean deleteById(int aliasId);
+ public interface AliasRepository {
+        Alias findByAgentId(int agentId);
+        Alias add(Alias alias);
+        boolean update(Alias agent);
+        @Transactional
+        boolean deleteById(int aliasId);
 
 }
